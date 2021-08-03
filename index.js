c1e8c313b85e91c173873dee1296d857ad654175
Dogedox <moefromsimpsonss@gmail.com>
2021-08-03 14:40:17 +0300

    models done

--- index.js
+++ index.js
@@ -4,15 +4,7 @@ require('dotenv').config();
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
+const Blog = require('./models/blog');
 
 const mongoUrl = process.env.MONGODB_URI;
 mongoose.connect(mongoUrl, {
